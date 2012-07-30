19f1d963820e90780cc1c11da8a65402a482f74c
Michael Niu <mike@myright.me>
2012-07-30 02:15:42 -0700

    New Files

---
+++ myappli/controllers/viewajax.php
@@ -0,0 +1,21 @@
+<?php
+class Viewajax extends CI_Controller {
+
+	public function index()
+	{
+		show_404();
+	}
+	
+	public function _remap($method)
+	{
+		$this->load->model('Viewmodel');
+		$data['method'] = $method;
+		$data['element'] = $this->Viewmodel->get_element($method);
+		$data['title'] = $data['element']->title;
+		$data['categories'] = $this->Viewmodel->get_categories();
+		$data['comments'] = $this->Viewmodel->get_comments($data['element']->id);
+		
+		$this->load->view('viewport_ajax', $data);
+	}
+}
+?>
